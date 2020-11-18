deb10ddf26b1db370ee3e0de9f12af7a843ccbb2
Lucas Lima <lucasl1m4x@gmail.com>
2020-11-18 00:01:30 -0300

    Atualizando o main
    Adicionado a ultima rota(final screen)

--- dsrpt21_app/lib/main.dart
+++ dsrpt21_app/lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:dsrpt21_app/screens/final_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:dsrpt21_app/screens/home_screen.dart';
 import 'package:dsrpt21_app/screens/progress_screen.dart';
+import 'package:dsrpt21_app/screens/final_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -15,6 +17,7 @@ class MyApp extends StatelessWidget {
       routes: {
         '/': (context) => HomeScreen(),
         '/progress_screen': (context) => ProgressScreen(),
+        '/final_screen': (context) => FinalScreen()
       },
     );
   }
